1c5111d2681ed9b809f4294f71d31889a6ec1dc9
Johannes Zauner <112665672+JZauner@users.noreply.github.com>
2026-01-27 13:37:24 +0100

    added add_label and add_labels

---
+++ scripts/add_label.R
@@ -0,0 +1,15 @@
+add_label <- function(data, label){
+  attr(data, "label") <- label
+  data
+}
+
+add_labels <- function(data, labels) {
+  stopifnot(is.data.frame(data))
+  stopifnot(is.character(labels) || is.list(labels))
+  
+  out <- purrr::imap(data, \(x, nm) {
+    if (nm %in% names(labels)) add_label(x, labels[[nm]]) else x
+  })
+  
+  tibble::as_tibble(out)   # or vctrs::list_cbind(out)
+}
